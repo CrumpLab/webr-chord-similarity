413c695ec21a24d6ea6b6e0e9dcffcb107ee8cba
Matt Crump <crumpmj@gmail.com>
2024-01-22 16:13:31 -0500

    updates

--- app/server.R
+++ app/server.R
@@ -1,6 +1,15 @@
 # Define server logic required to draw a histogram ----
 server <- function(input, output) {
 
+  # Download a dataset
+  download.file(
+    'https://raw.githubusercontent.com/crumplab/webr-chord-similarity/raw-data/main/first_order.csv',
+    'first_order.csv'
+  )
+
+  # Read the data
+  df_first_order = read.csv("first_order.csv")
+
   # Histogram of the Old Faithful Geyser Data ----
   # with requested number of bins
   # This expression that generates a histogram is wrapped in a call
@@ -11,7 +20,7 @@ server <- function(input, output) {
   # 2. Its output type is a plot
   output$distPlot <- renderPlot({
 
-    x    <- faithful$waiting
+    x    <- df_first_order$C
     bins <- seq(min(x), max(x), length.out = input$bins + 1)
 
     hist(x, breaks = bins, col = "#75AADB", border = "white",
